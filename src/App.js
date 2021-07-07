8e6fce57c9ef97d1535c7a24beb4c4585113f469
Jessica Moody <jessicamoody1988@gmail.com>
2021-07-07 11:58:24 -0500

    Initial Configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>J&J Promotions</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
